99b9d853dd3964ad6461deca7a757d2a4a736e30
luohonglin <1187239520@qq.com>
2019-10-18 23:16:34 +0800

    day08

---
+++ gmall-redisson-test/src/main/java/com/atguigu/gmall/gmallredissontest/GmallRedissonTestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.gmallredissontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallRedissonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallRedissonTestApplication.class, args);
+    }
+
+}
---
+++ gmall-redisson-test/src/test/java/com/atguigu/gmall/gmallredissontest/GmallRedissonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.gmallredissontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallRedissonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
